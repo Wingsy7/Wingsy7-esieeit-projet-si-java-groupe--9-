a842351b26b5783f7d05917e740a32c22283a056
Miroslav <cozma.miroslav2015@yandex.com>
2026-02-11 14:20:37 +0100

    Add App main class and first JUnit test

---
+++ app/src/test/java/com/esieeit/projetsi/AppTest.java
@@ -0,0 +1,11 @@
+package com.esieeit.projetsi;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void sanity_check() {
+        assertTrue(true, "Le projet est bien initialisé");
+    }
+}
